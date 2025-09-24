1f996666f8d8d0f6ecda3a1bb2b3b18f27881f44
sks260 <sks260@HA-0417-01>
2025-09-24 11:46:05 -0400

    New program for calculating discounts.

---
+++ RetirementIncome/CalculatingDiscounts.cpp
@@ -0,0 +1,27 @@
+// CalculatingDiscounts.cpp : 
+// This program shows how calculate a discounted price
+//
+
+// main.cpp : 
+// This program calculates and displays a discounted price
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double originalPrice = 59.95;
+	double discountPercentage = 20;
+
+	double discountAmount = originalPrice * discountPercentage / 100;
+
+	cout << "Original Price = $" << originalPrice << endl;
+	cout << "Discount Percentage = " << discountPercentage << "%" << endl;
+	cout << "Discount amount = $" << discountAmount << endl;
+	cout << "Discounted price = $" << (originalPrice - discountAmount) << endl;
+
+	return 0;
+
+}
